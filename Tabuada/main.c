1e5a199b37141fd17f67cfd873f788d43f7632a0
Gabriel Rodrigues de Rodrigues <gabriel.archangel369@gmail.com>
2022-09-06 11:54:01 -0300

    atualizando alguns projetos

--- Tabuada/main.c
+++ Tabuada/main.c
@@ -6,11 +6,11 @@ int main()
     int x;
     int y;
     int result;
-    printf("Enter with a valor to the m \n");
+    printf("Qual é a tabuada: \n");
     scanf("%d", &x);
     for(y = 0;y < 11; y++ ){
         result = y * x;
-        printf("valor %d\n", result);
+        printf("%2d x %1d = %2d\n", y, x, result);
     }
     return 0;
 }
